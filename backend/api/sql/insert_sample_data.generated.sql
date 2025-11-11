da2d1c17f6e745c6621c5402e99610c686831b9b
williamqin123 <willqin123@gmail.com>
2025-11-11 17:51:29 -0600

    Update insert_sample_data.generated.sql

--- backend/api/sql/insert_sample_data.generated.sql
+++ backend/api/sql/insert_sample_data.generated.sql
@@ -1,14 +1,14 @@
 
 
-INSERT INTO 'Ticker' ('company_name','country','industry','sector','ticker_symbol') VALUES ('Apple Inc.','USA','Consumer Electronics','Technology','AAPL');
-INSERT INTO 'Ticker' ('company_name','country','industry','sector','ticker_symbol') VALUES ('Amazon.com Inc.','USA','Internet Retail','Consumer','AMZN');
-INSERT INTO 'Ticker' ('company_name','country','industry','sector','ticker_symbol') VALUES ('Microsoft Corporation','USA','Software','Technology','MSFT');
-INSERT INTO 'Ticker' ('company_name','country','industry','sector','ticker_symbol') VALUES ('Netflix Inc.','USA','Entertainment','Communication Services','NFLX');
-INSERT INTO 'Ticker' ('company_name','country','industry','sector','ticker_symbol') VALUES ('Tesla Inc.','USA','Automobiles','Consumer','TSLA');
+INSERT INTO `Ticker` (`company_name`,`country`,`industry`,`sector`,`ticker_symbol`) VALUES ('Apple Inc.','USA','Consumer Electronics','Technology','AAPL');
+INSERT INTO `Ticker` (`company_name`,`country`,`industry`,`sector`,`ticker_symbol`) VALUES ('Amazon.com Inc.','USA','Internet Retail','Consumer','AMZN');
+INSERT INTO `Ticker` (`company_name`,`country`,`industry`,`sector`,`ticker_symbol`) VALUES ('Microsoft Corporation','USA','Software','Technology','MSFT');
+INSERT INTO `Ticker` (`company_name`,`country`,`industry`,`sector`,`ticker_symbol`) VALUES ('Netflix Inc.','USA','Entertainment','Communication Services','NFLX');
+INSERT INTO `Ticker` (`company_name`,`country`,`industry`,`sector`,`ticker_symbol`) VALUES ('Tesla Inc.','USA','Automobiles','Consumer','TSLA');
 
-INSERT INTO 'User' ('email','password_hash','first_name','last_name') VALUES ('rileylaurie@example.net','rhGEQq7eUVPEv4TwSvmiCZ69Ix0sE5MZTP9Yujt5mF0=','Kristin','Clark');
-INSERT INTO 'User' ('email','password_hash','first_name','last_name') VALUES ('jasonjones@example.net','JyK8xvV97GNVAeMxiD21nqneRrRK9ywoCVzXu4nozsw=','Brooke','Parker');
-INSERT INTO 'User' ('email','password_hash','first_name','last_name') VALUES ('danielleallen@example.net','hw4UaNxl9alXAcjCb8pNXXME/QLiti1sGFF5ecUOsGU=','Ashley','Henry');
-INSERT INTO 'User' ('email','password_hash','first_name','last_name') VALUES ('williammartinez@example.com','efaDo4NHpmDyxjHCVzaM1mPIZU4nM+hX20+zQ/9i2rw=','Tyler','Steele');
-INSERT INTO 'User' ('email','password_hash','first_name','last_name') VALUES ('bensonjames@example.net','wiiO/B81Njr1SsXwK1j/xBTis99uy1rXqH0RcGoL6VE=','Shannon','Ruiz');
+INSERT INTO `User` (`email`,`password_hash`,`first_name`,`last_name`) VALUES ('rileylaurie@example.net','rhGEQq7eUVPEv4TwSvmiCZ69Ix0sE5MZTP9Yujt5mF0=','Kristin','Clark');
+INSERT INTO `User` (`email`,`password_hash`,`first_name`,`last_name`) VALUES ('jasonjones@example.net','JyK8xvV97GNVAeMxiD21nqneRrRK9ywoCVzXu4nozsw=','Brooke','Parker');
+INSERT INTO `User` (`email`,`password_hash`,`first_name`,`last_name`) VALUES ('danielleallen@example.net','hw4UaNxl9alXAcjCb8pNXXME/QLiti1sGFF5ecUOsGU=','Ashley','Henry');
+INSERT INTO `User` (`email`,`password_hash`,`first_name`,`last_name`) VALUES ('williammartinez@example.com','efaDo4NHpmDyxjHCVzaM1mPIZU4nM+hX20+zQ/9i2rw=','Tyler','Steele');
+INSERT INTO `User` (`email`,`password_hash`,`first_name`,`last_name`) VALUES ('bensonjames@example.net','wiiO/B81Njr1SsXwK1j/xBTis99uy1rXqH0RcGoL6VE=','Shannon','Ruiz');
 
