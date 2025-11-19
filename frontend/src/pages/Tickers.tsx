56d9958e93c06a7755f8b6a341219c2c2761c2ef
williamqin123 <willqin123@gmail.com>
2025-11-18 23:39:05 -0600

    turn on prettier code formatter

--- frontend/src/pages/Tickers.tsx
+++ frontend/src/pages/Tickers.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import {
   Table,
   TableBody,
@@ -10,80 +10,87 @@ import {
   TableRow,
 } from "../components/ui/table";
 import { Input } from "../components/ui/input";
-import { useState, useEffect, useContext } from 'react';
-import { UserContext } from '@/context/ActiveUserContext';
-import { ActionFeedbackToastsContext } from '@/context/ActionFeedbackToastsContext';
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "@/context/ActiveUserContext";
+import { ActionFeedbackToastsContext } from "@/context/ActionFeedbackToastsContext";
 
-import { apiCall } from '../App';
+import { apiCall } from "../App";
 
 interface Ticker {
-    tickerSymbol: string;
-    company: string;
-    lastPrice: number;
+  tickerSymbol: string;
+  company: string;
+  lastPrice: number;
 }
 
 function Tickers() {
+  const [currentTickers, setCurrentTickers] = useState<Ticker[]>([]);
+  const [searchBarValue, setSearchBarValue] = useState<string>("");
 
-    const [currentTickers, setCurrentTickers] = useState<Ticker[]>([]);
-    const [searchBarValue, setSearchBarValue] = useState<string>('');
+  const activeUserContext = useContext(UserContext);
+  const actionFeedbackToastsContext = useContext(ActionFeedbackToastsContext);
 
-    const activeUserContext = useContext(UserContext);
-    const actionFeedbackToastsContext = useContext(ActionFeedbackToastsContext);
+  async function queryTickers() {
+    apiCall(
+      activeUserContext,
+      actionFeedbackToastsContext,
+      {
+        endpoint: "tickers",
+        method: "GET",
+        params: {
+          ...(searchBarValue.length > 0 && { search_query: searchBarValue }),
+        },
+      },
+      (data) => {
+        setCurrentTickers(data);
+      },
+      false
+    );
+  }
 
-    async function queryTickers() {
-        apiCall(activeUserContext, actionFeedbackToastsContext, {
-            endpoint: 'tickers',
-            method: 'GET',
-            params: {
-                ...(searchBarValue.length > 0 && {'search_query': searchBarValue})
-            }
-        }, (data) => {
-            setCurrentTickers(data);
-        }, false);
-    }
+  // This effect runs whenever 'searchQuery' changes
+  useEffect(() => {
+    queryTickers();
+  }, [searchBarValue]);
 
-    // This effect runs whenever 'searchQuery' changes
-    useEffect(() => {
-        queryTickers();
-    }, [searchBarValue])
+  useEffect(() => {
+    queryTickers();
+  }, []); // The empty dependency array ensures this effect runs only once on mount
 
-    useEffect(() => {
-        queryTickers();
-    }, []); // The empty dependency array ensures this effect runs only once on mount
+  function onSearchBarChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearchBarValue((event.target as HTMLInputElement)?.value);
 
-    function onSearchBarChange(event: React.ChangeEvent<HTMLInputElement>) {
-        setSearchBarValue((event.target as HTMLInputElement)?.value);
-
-        // queryTickers();
-    }
+    // queryTickers();
+  }
 
   return (
     <main>
-        <h1 className='text-4xl font-bold mb-4'>Tickers</h1>
-        <div className='mb-5 grid grid-cols-[auto_1fr] gap-5'>
-            <span className='py-1 font-medium'>Search:</span>
-            <Input onChange={onSearchBarChange} value={searchBarValue}></Input>
-        </div>
-        <Table>
+      <h1 className="text-4xl font-bold mb-4">Tickers</h1>
+      <div className="mb-5 grid grid-cols-[auto_1fr] gap-5">
+        <span className="py-1 font-medium">Search:</span>
+        <Input onChange={onSearchBarChange} value={searchBarValue}></Input>
+      </div>
+      <Table>
         <TableHeader>
-            <TableRow>
+          <TableRow>
             <TableHead className="w-[100px]">Ticker Symbol</TableHead>
             <TableHead>Company Name</TableHead>
             <TableHead className="text-right">Last Price</TableHead>
-            </TableRow>
+          </TableRow>
         </TableHeader>
         <TableBody>
-            {currentTickers.map((ticker) => (
+          {currentTickers.map((ticker) => (
             <TableRow key={ticker.tickerSymbol}>
-                <TableCell className="font-medium">
-                    <Link to={`/pricehistory/${ticker.tickerSymbol}`}>{ticker.tickerSymbol}</Link>
-                </TableCell>
-                <TableCell>{ticker.company}</TableCell>
-                <TableCell className="text-right">{ticker.lastPrice}</TableCell>
+              <TableCell className="font-medium">
+                <Link to={`/pricehistory/${ticker.tickerSymbol}`}>
+                  {ticker.tickerSymbol}
+                </Link>
+              </TableCell>
+              <TableCell>{ticker.company}</TableCell>
+              <TableCell className="text-right">{ticker.lastPrice}</TableCell>
             </TableRow>
-            ))}
+          ))}
         </TableBody>
-        </Table>
+      </Table>
     </main>
   );
 }
