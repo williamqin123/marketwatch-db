11f3b8dcf8f1aa8e0431917a663a3769ca2c50e3
williamqin123 <willqin123@gmail.com>
2025-11-18 22:44:15 -0600

    landing page image + make scrolling possible + add developers names

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,11 +1,19 @@
 import { Button } from "../components/ui/button";
 import { Link } from 'react-router-dom';
 
+import hero from '@/assets/hero.svg';
+
 
 function Landing() {
   return (
     <main>
-        <h1 className="text-5xl my-5">MarketWatch DB Project</h1>
+        <h1 className="text-5xl mt-10 mb-5">MarketWatch DB Project</h1>
+        <p>
+            By Alexia A, Brayton B, Daniel V, &amp; William Q
+        </p>
+        <div className='flex place-items-center my-20' style={{height: 'calc(10rem + 15vh + 5vw)'}}>
+            <img src={hero} alt="" className='h-full w-full' style={{objectFit: 'contain'}} />
+        </div>
         <div className="grid grid-cols-1 gap-2 md:grid-cols-3">
             <Button asChild className="!text-white">
                 <a href="https://github.com/Daniel6278/marketwatch-db" target="_blank">GitHub Repo</a>
